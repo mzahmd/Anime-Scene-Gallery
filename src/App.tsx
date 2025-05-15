018b1894d510e78c9130b2237c02fc7ed4416c78
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-05-15 16:06:25 +0200

    installed tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div className="underline hover:text-red-400">
+      <h1>Dragonball</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
