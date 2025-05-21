e2740d9359895079881bf2f20584b08022625fdc
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-05-21 19:15:45 +0200

    hover effect

--- src/components/AnimeTitle.tsx
+++ src/components/AnimeTitle.tsx
@@ -7,9 +7,10 @@ interface AnimeTitleProps {
 export function AnimeTitle({ title }: AnimeTitleProps) {
   return (
     <motion.span
-      className="font-bold text-7xl text-white pb-2 transition ease-in-out hover:text-gray-400"
+      className="font-bold text-7xl text-white pb-2 transition-colors duration-300 ease-in-out hover:text-gray-400"
       animate={{}}
-      whileHover={{}}
+      whileHover={{ scaleY: 1.2 }}
+      transition={{ type: "spring", mass: 0.5, damping: 5 }}
     >
       {title}
     </motion.span>
