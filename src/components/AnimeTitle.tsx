9ab657a20e7bd795b36fc00c6cea302e5da686c5
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-05-21 20:11:09 +0200

    add data-text to store it in event

--- src/components/AnimeTitle.tsx
+++ src/components/AnimeTitle.tsx
@@ -7,10 +7,12 @@ interface AnimeTitleProps {
 export function AnimeTitle({ title }: AnimeTitleProps) {
   return (
     <motion.span
+      data-text={title}
       className="font-bold text-7xl text-slate-200 pb-2 transition-colors duration-300 ease-in-out hover:text-slate-400"
       animate={{}}
       whileHover={{ scaleY: 1.2 }}
       transition={{ type: "spring", mass: 0.5, damping: 5 }}
+      onClick={(e) => console.log(e.currentTarget.dataset.text)}
     >
       {title}
     </motion.span>
