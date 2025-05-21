ee24063c4c9964fe8ec7cd82ca4358ba2cef1d32
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-05-21 18:45:26 +0200

    refactor

--- src/components/AnimeTitle.tsx
+++ src/components/AnimeTitle.tsx
@@ -4,14 +4,14 @@ interface AnimeTitleProps {
   title: string;
 }
 
-export function AnimeTitle({title}: AnimeTitleProps) {
+export function AnimeTitle({ title }: AnimeTitleProps) {
   return (
     <motion.span
       className="font-bold text-7xl text-white pb-2 transition ease-in-out hover:text-gray-400"
       animate={{}}
       whileHover={{}}
-      >
-        {title}
+    >
+      {title}
     </motion.span>
   )
-} 
+}
