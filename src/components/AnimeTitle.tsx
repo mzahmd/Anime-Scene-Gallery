4434fca6cdfc3a96beb69860d02a94e8c51072b7
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-05-21 19:20:27 +0200

    changed text color

--- src/components/AnimeTitle.tsx
+++ src/components/AnimeTitle.tsx
@@ -7,7 +7,7 @@ interface AnimeTitleProps {
 export function AnimeTitle({ title }: AnimeTitleProps) {
   return (
     <motion.span
-      className="font-bold text-7xl text-white pb-2 transition-colors duration-300 ease-in-out hover:text-gray-400"
+      className="font-bold text-7xl text-slate-200 pb-2 transition-colors duration-300 ease-in-out hover:text-slate-400"
       animate={{}}
       whileHover={{ scaleY: 1.2 }}
       transition={{ type: "spring", mass: 0.5, damping: 5 }}
