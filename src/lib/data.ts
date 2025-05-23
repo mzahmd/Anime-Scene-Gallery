7935b9ea92b10f5f9da868f95578d126bffc61d0
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-05-23 19:55:39 +0200

    starting animation for multiple anime

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,6 @@
 import p1 from "../tmp/p1.png";
 import p2 from "../tmp/p2.png";
 import p3 from "../tmp/p3.png";
-import p4 from "../tmp/p4.png";
 
 export interface Image {
   src: string;
@@ -38,27 +37,50 @@ export const data: Record<string, Image[]> = {
         y: -115,
       },
     },
-    // {
-    //   src: p3,
-    //   initial: {
-    //     x: 100,
-    //     y: 140,
-    //   },
-    //   animate: {
-    //     x: 120,
-    //     y: 145,
-    //   },
-    // },
-    // {
-    //   src: p4,
-    //   initial: {
-    //     x: 100,
-    //     y: 100,
-    //   },
-    //   animate: {
-    //     x: 120,
-    //     y: 100,
-    //   },
-    // },
+    {
+      src: p3,
+      initial: {
+        x: 100,
+        y: 140,
+      },
+      animate: {
+        x: 120,
+        y: 145,
+      },
+    },
+  ],
+  Naruto: [
+    {
+      src: p1,
+      initial: {
+        x: 100,
+        y: 100,
+      },
+      animate: {
+        x: 110,
+      },
+    },
+    {
+      src: p2,
+      initial: {
+        x: 400,
+        y: -120,
+      },
+      animate: {
+        x: 420,
+        y: -115,
+      },
+    },
+    {
+      src: p3,
+      initial: {
+        x: 400,
+        y: -120,
+      },
+      animate: {
+        x: 420,
+        y: -115,
+      },
+    },
   ],
 };
