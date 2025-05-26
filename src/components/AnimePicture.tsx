bed38b59484c2267beca032db70b85986dc091dc
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-05-26 16:54:20 +0200

    refactor

--- src/components/AnimePicture.tsx
+++ src/components/AnimePicture.tsx
@@ -11,8 +11,17 @@ export function AnimePicture({ title, item }: AnimePictureProps) {
   return (
     <motion.div
       className="absolute w-75 overflow-hidden top-70 left-20 rounded-2xl rotate-355"
-      initial={{ opacity: 0, x: item.initial.x, y: item.initial.y }}
-      animate={{ opacity: 1, rotate: item.animate.rotate, x: item.animate.x, y: item.animate.y }}
+      initial={{
+        opacity: 0,
+        x: item.initial.x,
+        y: item.initial.y
+      }}
+      animate={{
+        opacity: 1,
+        rotate: item.animate.rotate,
+        x: item.animate.x,
+        y: item.animate.y
+      }}
       exit={{ opacity: 0 }}
     >
       <img src={item.src} alt={title} className="object-cover" />
